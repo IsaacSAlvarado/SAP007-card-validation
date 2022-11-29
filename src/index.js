989772b5214b2f9e384d92ac9e38f590d11ca73c
Jairo Alvarado <jairoalvarado@MacBook-Air-de-Jairo.local>
2022-11-29 15:32:07 -0500

    PROYECTO SEMIFINALIZADO - FALTA CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,105 @@
 import validator from './validator.js';
+//console.log(validator);
 
-console.log(validator);
+// Esto permite ver la primera página y validar si el ususario colocó el correo o no.
+// Permite ver si el correo ingresado cumple con la expresión correcta, de no hacerlo devuelve un mensaje.
+// Al ingresar el correo válido y hacer click en el botón get started nos deja avanzar a la segunda página.
+const button1 = document.getElementById("getstarted");
+button1.addEventListener("click", email);
+
+function email (){
+    const email = document.getElementById ("email");
+    const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    let message = ""
+    if (email.value === ""){
+        email.classList.add ("noemail")
+        message = " Email is required!"
+    } else if (email.value.match (regex)=== null) {
+        email.classList.add ("noemail")
+        message = "Ingrese un correo válido."
+    }  else {
+            document.getElementById ("one").style.display = "none"
+            document.getElementById ("two").style.display = "block"
+    }
+    document.getElementById ("empty").innerHTML = message;
+} 
+
+//Nos permite seleccionar el plan individual e ir a la página de elegir el método de pago.
+const button2 = document.getElementById ("getstarted1");
+button2.addEventListener ("click", pay )
+
+//Nos permite seleccionar el plan Duo e ir a la página de elegir el método de pago.
+
+const button3 = document.getElementById ("getstarted2");
+button3.addEventListener ("click", pay)
+
+//Nos permite seleccionar el plan Familiar e ir a la página de elegir el método de pago.
+
+const button4 = document.getElementById ("getstarted3");
+button4.addEventListener ("click", pay )
+
+function pay (){
+    document.getElementById ("two").style.display = "none"
+    document.getElementById ("three").style.display = "block"
+}
+
+//Aquí está la función que nos permite modificar los dígitos colocados por el usuario en = # excepto los últimos 4 dígitos. 
+let number = ""
+
+const card = document.getElementById ("cardNumber");
+card.addEventListener ("change", (e)=> {
+    e.preventDefault()
+number = e.target.value 
+//console.log (number)
+card.value = validator.maskify (number) 
+
+const number1 = validator.isValid (number)
+    // console.log (validator.isValid (number))
+    let message1 = ""
+        if (number1 === true){
+            //cardNumber.classList.add ("noValid")
+            message1 = "Your card is valid!"
+        } else if (number === "") {
+            // email.classList.add ("noemail")
+            message1 = "Type a card number";
+        }  else if(number1 === false) {
+        //cardNumber.classList.add ("novalid")
+            message1 = "Invalid card! Please type a valid card number."; 
+        }
+    document.getElementById ("validInvalidCard").innerHTML = message1;
+    return message1
+
+})
+
+// Esta función nos permite detener el curso natural del botón submit.
+const submit= document.getElementById ("submit");
+submit.addEventListener ("click", (e)=> { 
+e.preventDefault()
+
+const number1 = validator.isValid (number)
+    // console.log (validator.isValid (number))
+    let message1 = ""
+        if (number1 === true){
+            //cardNumber.classList.add ("noValid")
+            message1 = "Your card is valid!"
+        } else if (number === "") {
+            // email.classList.add ("noemail")
+            message1 = "Type a card number";
+        }  else if(number1 === false) {
+        //cardNumber.classList.add ("novalid")
+            message1 = "Invalid card! Please type a valid card number."; 
+        }
+    document.getElementById ("validInvalidCard").innerHTML = message1;
+    return message1
+
+})
+
+
+
+
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,63 @@
 const validator = {
-  // ...
-};
+  isValid: function (number) {
+    const arr = number.split ("")
+    //console.log (arr)
+    const arrr = arr.reverse ();
+    //console.log (arrr)  
+    let finalNumber = arrr
+    for ( let i = 1; i < arrr.length; i=i+2) {
+      arrr [i] = arrr[i]*2
+      //console.log (finalNumber) 
+    } 
+
+    let addNumber = finalNumber
+    for (let i = 1; i < finalNumber.length; i= i+2){
+      if (finalNumber[i] > 9) {
+        finalNumber[i] = finalNumber[i] - 9;
+        //console.log (addNumber)
+      }
+    } 
+
+    let suma = addNumber.reduce ((accumulator, currentValue) => 
+      Number (accumulator) + Number (currentValue), 0
+    );
+    //console.log (suma)
+
+
+    let lastNumber = suma //%= 10;
+    // let lastNumber = suma.toString().slice(-1);
+    //console.log (lastNumber)
+    // if (lastNumber === "0")
+    return (lastNumber%10 !== 0 || lastNumber == "" ? false:true)
+// Si el residuo de la división es === 0 - true
+// Si está vacío el campo de la división === false 
+// Si la división no es === 0 - false 
+  //   if (lastNumber%10 === 0){
+  //       return true
+  //       console.log (lastNumber)
+  //   } else {
+  //       return false
+  // }
+},
+
+  maskify: function (number) {
+
+  const change = number.split ("")
+  //console.log (change)
+
+  let visible = []
+  change.forEach((item, i) => {
+      if (i<change.length-4){
+        item = "#"
+      }
+      //console.log (item)
+  visible.push (item)
+      //console.log (visible)
+    });
+      //console.log (visible.join (''))
+      return visible.join ('');
+  }
+        
+}
 
 export default validator;
